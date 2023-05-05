2500af9f696c033df92267be3b01737a7b76f94f
ahmedmihdawy <ahmedmihdawy@gmail.com>
2023-05-05 15:37:23 +0300

    shering data across request

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const path = require('path');
-
 const express = require('express');
-
 const rootDir = require('../util/path');
-
 const router = express.Router();
+
+
+const product =[];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -14,7 +14,11 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  product.push({title:req.body.title})
   res.redirect('/');
 });
 
-module.exports = router;
+
+
+exports.router = router;
+exports.product = product;
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,10 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin') 
+
 router.get('/', (req, res, next) => {
+  console.log(adminData.product)
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
