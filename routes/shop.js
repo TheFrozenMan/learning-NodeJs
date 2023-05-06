17ec8416727793a27d33ea759fc746d4b39b32a4
ahmedmihdawy <ahmedmihdawy@gmail.com>
2023-05-06 17:02:16 +0300

    installing the pug librarry

--- routes/shop.js
+++ routes/shop.js
@@ -8,11 +8,15 @@ const router = express.Router();
 
 const adminData = require('./admin') 
 
-router.get('/', (req, res, next) => {
-  console.log(adminData.product)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+// router.get('/', (req, res, next) => {
+//   console.log(adminData.product)
+//   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+// });
 
+
+router.get('/', (req, res, next) => {
+  res.render('shop');
+});
 
 
 module.exports = router;
