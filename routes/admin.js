f953073c3f1309f4b9eed74975ef24d42fad09d7
ahmedmihdawy <ahmedmihdawy@gmail.com>
2023-05-29 08:32:33 +0300

    adding-product

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,9 @@ const product =[];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product',{pageTitle:'add-product', path :'/admin/add-product'})
+  res.render('add-product',
+  {
+    pageTitle:'add-product', path :'/admin/add-product', formsCSS : true , productCSS : true , addproductActive : true })
 });
 
 // /admin/add-product => POST
