9aeb17b31c403bb008c990195680d699d98e079e
ahmedmihdawy <ahmedmihdawy@gmail.com>
2023-06-02 14:42:07 +0300

    adding some note

--- routes/shop.js
+++ routes/shop.js
@@ -4,14 +4,16 @@ const router = express.Router();
 const productsController  = require ('../controllers/porducts')
 
 // routing for the mean page 
+// / => GET
 router.get('/', productsController.getProuducts);
 
-// routing for the cart 
+// routing for the cart  
+// /cart => GET
 router.get('/cart',productsController.getTheCart) ;
 
 
 // routig for the product
-
+// /cart => GET
 router.get('/product',productsController.getTheProductDeatiles)
 
 module.exports = router;
