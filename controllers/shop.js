23439898d66cb6bedf85d311b0d1a292878ebea1
ahmad mihd <ahmedmihdawy@gmail.com>
2023-07-24 23:34:13 +0300

    geting the data form the data base

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,13 +23,18 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll().then(
+    ([rows,filedData]) =>{
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    }
+  ).catch(err=>{console.error(err) ;}); 
+  
+  
+  
 };
 
 exports.getCart = (req, res, next) => {
