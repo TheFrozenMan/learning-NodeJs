56e76f8befe681b2f8e12a67f00962223905ace1
ahmad mihd <ahmedmihdawy@gmail.com>
2023-07-27 11:42:03 +0300

    :grin: creating a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then( ()=>{
-    res.redirect('/');
-  }).catch(
-    err =>{
-      console.error(err)
-    }
-  );
+  Product.create({
+    title : title , 
+    price: price, 
+    imageUrl : imageUrl , 
+    description : description 
+  }).then( result =>{
+    // console.log(result);
+    console.log('created product')
+  }).catch(err=>{
+    console.error(err)
+  })
 };
 
 exports.getEditProduct = (req, res, next) => {
