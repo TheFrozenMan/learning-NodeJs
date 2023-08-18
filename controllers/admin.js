8bc5aec1bba8479b42183fae8897a4ef31b5a5a0
ahmad mihd <ahmedmihdawy@gmail.com>
2023-08-18 14:17:16 +0300

    fxing

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,17 +32,17 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
+  Product.findByPK(prodId).then(product=>{
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
     });
-  });
+  }).catch(err=>{
+    console.error(err);
+  })
+ 
 };
 
 exports.postEditProduct = (req, res, next) => {
